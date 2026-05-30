e6d55db816df00399086904e792cd77563907355
Lereko Seholoba <seholobalereko@gmail.com>
2026-05-31 00:15:56 +0200

    feat: debugged some code on program.cs file

--- CareerHub_API/Program.cs
+++ CareerHub_API/Program.cs
@@ -7,11 +7,11 @@ using CareerHub_API.Middleware;
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
-builder.Host.UseSerilog();
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
+
 
 builder.Services.AddControllers()
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
     .AddJsonOptions(options =>
     {
         // Return enums as strings instead of numbers
@@ -25,6 +25,8 @@ builder.Services.AddSingleton<JobService>();
 builder.Services.AddProblemDetails();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 var app = builder.Build();
 
