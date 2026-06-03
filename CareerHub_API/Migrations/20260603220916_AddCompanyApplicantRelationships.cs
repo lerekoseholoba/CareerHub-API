808276634819e01b2954fed07f46e3eba68576a9
Lereko Seholoba <seholobalereko@gmail.com>
2026-06-04 00:48:22 +0200

    feat: debugged

--- CareerHub_API/Migrations/20260603220916_AddCompanyApplicantRelationships.cs
+++ CareerHub_API/Migrations/20260603220916_AddCompanyApplicantRelationships.cs
@@ -22,7 +22,7 @@ namespace CareerHub_API.Migrations
             migrationBuilder.AddColumn<Guid>(
                 name: "CompanyId",
                 table: "job_listings",
-                type: "uuid",
+                type: "TEXT",
                 nullable: false,
                 defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
 
