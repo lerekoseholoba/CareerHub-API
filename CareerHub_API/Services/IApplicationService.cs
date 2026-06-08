d0604197ac18b453ffa2b45470a2c62aa031f997
Lereko Seholoba <seholobalereko@gmail.com>
2026-06-08 21:52:54 +0200

    Fix IApplicationService: add missing using statements and update method signatures

--- CareerHub_API/Services/IApplicationService.cs
+++ CareerHub_API/Services/IApplicationService.cs
@@ -1,9 +1,13 @@
+using CareerHub_API.DTOs;
+using CareerHub_API.Models;
+
 namespace CareerHub_API.Services
 {
     public interface IApplicationService
     {
-        Task SubmitApplicationAsync(CreateApplicationRequest request);
-        Task UpdateStatusAsync(Guid applicantId, Guid jobId, ApplicationStatus status);
-        Task WithdrawAsync(Guid applicantId, Guid jobId, Guid currentUserApplicantId);
+        Task<ServiceResult<Application>> ApplyToJobAsync(CreateApplicationRequest request);
+        Task<ServiceResult<List<Application>>> GetApplicationsForJobAsync(Guid jobId);
+        Task<ServiceResult<List<Application>>> GetApplicationsForUserAsync(Guid userId);
+        Task<ServiceResult<string>> WithdrawApplicationAsync(Guid applicationId);
     }
 }
