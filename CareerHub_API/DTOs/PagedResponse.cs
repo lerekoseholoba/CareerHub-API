11f400d4689a9fae79f92ecac061d4abe7fd5684
Lereko Seholoba <seholobalereko@gmail.com>
2026-06-09 05:21:59 +0200

    feat: updated code and expanded seed data for pagination

---
+++ CareerHub_API/DTOs/PagedResponse.cs
@@ -0,0 +1,20 @@
+namespace CareerHub_API.DTOs;
+public record PagedResponse<T>
+{
+    public IEnumerable<T> Data { get; init; } = [];
+
+    public int Page { get; init; }
+
+    public int PageSize { get; init; }
+
+    public int TotalCount { get; init; }
+
+    public int TotalPages =>
+        (int)Math.Ceiling((double)TotalCount / PageSize);
+
+    public bool HasNextPage =>
+        Page < TotalPages;
+
+    public bool HasPreviousPage =>
+        Page > 1;
+}
